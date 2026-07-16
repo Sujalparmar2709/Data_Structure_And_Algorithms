0913720b64c13caada708f535c9cd2b7c37ef05f
Sujal Parmar <sujalparmar2709@gmail.com>
2026-07-16 12:06:37 +0530

    Update smallest and largest or second smallest and second largest.cpp

--- smallest and largest or second smallest and second largest.cpp
+++ smallest and largest or second smallest and second largest.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[] = {1,2,3,4,5,6,7};
+    int arr[] = {5,2,1,6,3,8};
     int size = sizeof(arr)/sizeof(arr[0]);
     
     int smallest = arr[0];
